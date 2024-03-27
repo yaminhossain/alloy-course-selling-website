9adbea4675a46ab3d3a536c8b15dddf3cf39971d
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2024-03-28 04:14:39 +0600

    login page branch setup done

--- alloy-course-selling-website/src/Pages/Home/Home/Home.jsx
+++ alloy-course-selling-website/src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
     return (
         <div >
              <h2 className="text-5xl text-center">Home</h2>
+             <h1>Hi Anik I have create a branch for Home page</h1>
         </div>
     );
 };
