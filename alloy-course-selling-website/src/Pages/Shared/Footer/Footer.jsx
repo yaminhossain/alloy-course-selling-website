ce0fff46c97f0c1414b79c093f1e34bdcb036839
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2024-03-29 03:18:19 +0600

    start working with footer

--- alloy-course-selling-website/src/Pages/Shared/Footer/Footer.jsx
+++ alloy-course-selling-website/src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,15 @@
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 
 const Footer = () => {
-    return (
-        <div className="bg-yellow-400 h-40 mx-10 text-center ">
-            <h2>This is footer</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="w-full border-2 py-5 text-white bg-gray-900 m-0"></div>
+      <div className="w-full border-2 text-center py-5 text-white bg-gray-800 m-0">
+      
+        <p> alloy.com Limited. All right reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
