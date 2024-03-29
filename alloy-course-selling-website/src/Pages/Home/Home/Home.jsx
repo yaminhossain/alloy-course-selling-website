9b73bbd54c234832e2f0f3a721d1eb4b423cd110
anik12136 <anikbiswas431@gmail.com>
2024-03-29 21:40:04 +0600

    home page start

--- alloy-course-selling-website/src/Pages/Home/Home/Home.jsx
+++ alloy-course-selling-website/src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,18 @@
+import Banner from "../Banner/Banner";
+import FeaturedTopicByCategory from "../FeaturedTopicByCategory/FeaturedTopicByCategory";
+import PopularCourses from "../PopularCourses/PopularCourses";
+import TopCategories from "../TopCategories/TopCategories";
+
 
 const Home = () => {
     return (
         <div >
-             <h2 className="text-5xl text-center">Home</h2>
+            <Banner></Banner>
+            <div className="mx-40">
+                <TopCategories></TopCategories>
+                <FeaturedTopicByCategory></FeaturedTopicByCategory>
+                <PopularCourses></PopularCourses>
+            </div>
         </div>
     );
 };
