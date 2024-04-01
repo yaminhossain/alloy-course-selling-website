b81f30a26c384b857a76876e28e46c625e043ce5
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2024-04-01 16:55:59 +0600

    fast-forward error

--- alloy-course-selling-website/src/Pages/Home/Home/Home.jsx
+++ alloy-course-selling-website/src/Pages/Home/Home/Home.jsx
@@ -2,7 +2,11 @@ import Banner from "../Banner/Banner";
 import FeaturedTopicByCategory from "../FeaturedTopicByCategory/FeaturedTopicByCategory";
 import InstructorJourney from "../InstructorJourney/InstructorJourney";
 import JoinOurCommunity from "../JoinOurCommunity/JoinOurCommunity";
+<<<<<<< HEAD
 import RequestCourses from "../../Home/RequestCourses/RequestCourses";
+=======
+import RequestCourses from "../RequestCourses /RequestCourses";
+>>>>>>> fd16701c077caea96c3f2e76687ba2c3a1b8b4a3
 import PopularCourses from "../PopularCourses/PopularCourses";
 import SchoolAndColleges from "../SchoolAndColleges/SchoolAndColleges";
 import Sponsor from "../Sponsor/Sponsor";
